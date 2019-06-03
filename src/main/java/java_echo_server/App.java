e265d1562912d573df12ff36522be63d972d8cbe
serpient <littlegangwolf@gmail.com>
2019-06-03 11:15:44 -0700

    Update App to control stopping of Echo Server
    - Stopping Server requires closing the ClientSocket and ServerSocket, which only App knows about at this point. Let App control closing the Server, vs, passing in references of ClientSocket and ServerSocket into echoServer

--- src/main/java/java_echo_server/App.java
+++ src/main/java/java_echo_server/App.java
@@ -8,34 +8,50 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+
     public static void main(String[] args) {
         int portNumber = setPortNumber(args);
         Client client = createClientSocketWrapper(portNumber);
 
         EchoServer echoServer = new EchoServer(client);
 
-        echoServer.start();
+        do {
+            echoServer.start();
+        } while (echoServer.isRunning());
+        stopServer();
+    }
+
+    private static int setPortNumber(String[] terminal_args) {
+        return terminal_args.length > 0 ? Integer.parseInt(terminal_args[0]) : 1234;
     }
 
     private static Client createClientSocketWrapper(int port) {
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
 
-            Boolean autoFlushPrintWriter = true;
-            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), autoFlushPrintWriter);
-
+            Boolean autoFlushWriter = true;
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), autoFlushWriter);
             WriterWrapper outputStream = new PrintWriterWrapper(printWriter);
             BufferedReader inputStream = new BufferedReader((new InputStreamReader(clientSocket.getInputStream())));
 
-            return new Client(inputStream, outputStream, clientSocket);
+            return new Client(inputStream, outputStream);
+
         } catch (IOException e) {
             System.err.println(e);
             return null;
         }
     }
 
-    private static int setPortNumber(String[] terminal_args) {
-        return terminal_args.length > 0 ? Integer.parseInt(terminal_args[0]) : 1234;
+    private static void stopServer() {
+        try {
+            System.out.println("The server is shutting down.");
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            System.err.println("The server could not be shut down.");
+        }
     }
 }
